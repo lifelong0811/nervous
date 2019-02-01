0b01c4dc6ebc01bc205bf0e086634b2b9429a098
wangleijie <lifelong0811@126.com>
2019-02-01 10:52:43 +0800

    nervous项目初始化完成

---
+++ src/main/java/org/nervous/nervous/NervousApplication.java
@@ -0,0 +1,14 @@
+package org.nervous.nervous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NervousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NervousApplication.class, args);
+	}
+
+}
+
